c4e88b4d978c8f38e6aa3652be7d6f597f307371
PratapreddyDS <98898318+PratapreddyDS@users.noreply.github.com>
2022-12-16 13:14:31 +0530

    Update spyDataBase.sql
    Spy database updated after increasing the length of some data types

--- spyDataBase.sql
+++ spyDataBase.sql
@@ -11,26 +11,25 @@ DROP TABLE AlphaAgents;
 DROP TABLE BetaAgents;
 DROP TABLE OperationDirector;
 DROP TABLE SkillSet;
-
 DROP TABLE WeaponsData;
-
 
 --
 CREATE TABLE BetaAgents
-        (EmpID varchar(15),
-        Names varchar(155),
-        Aliases varchar(155),
+        (EmpID varchar(156),
+        Names varchar(256),
+        Aliases varchar(256),
         Salary numeric(10,0),
-        SupervisorName varchar(155),
-        PassportNo varchar(155),
-        MotherTongue varchar(11),
+        SupervisorName varchar(256),
+        PassportNo varchar(256),
+        MotherTongue varchar(155),
         knownLanguagesCount numeric(3,0),
+        passwords varchar(255),
         primary key (EmpID)
         );
 --
 CREATE TABLE SkillSet
-        (SkillID varchar(15),
-        NameOfTheSkill varchar(155),
+        (SkillID varchar(156),
+        NameOfTheSkill varchar(256),
         RequireWeapons varchar(3),
         DaysToMaster numeric(4,0),
         mustLearn varchar(3),
@@ -39,8 +38,8 @@ CREATE TABLE SkillSet
 
 --
 CREATE TABLE WeaponsData
-        (SerialNo varchar(15),
-        NameOfTheWeapon varchar(155),
+        (SerialNo varchar(156),
+        NameOfTheWeapon varchar(256),
         SafeToUse varchar(3),
         needAssistanceToOperate varchar(3),
         RequiresExperienceToOperate varchar(3),
@@ -48,21 +47,22 @@ CREATE TABLE WeaponsData
         );
 --
 CREATE TABLE AlphaAgents
-        (EmpID varchar(15),
-        names varchar(155),
-        aliases varchar(155),
+        (EmpID varchar(156),
+        names varchar(256),
+        aliases varchar(256),
         Salary numeric(10,0),
-        SupervisorName varchar(155),
-        DriversLicence varchar(155),
-        Ethnicity varchar(11),
-        height varchar(11),
-        MotherTongue varchar(11),
-        identificationMarks varchar(11),
-        PassportNo varchar(11),
-        -- languageID varchar(11),
-        SkillID varchar(11),
-        -- OperationId varchar(11),
-        weaponID varchar(15),
+        SupervisorName varchar(256),
+        DriversLicence varchar(256),
+        Ethnicity varchar(155),
+        height numeric(4),
+        MotherTongue varchar(155),
+        identificationMarks varchar(155),
+        PassportNo varchar(155),
+        passwords varchar(255),
+        -- languageID varchar(155),
+        SkillID varchar(155),
+        -- OperationId varchar(155),
+        weaponID varchar(156),
         primary key (EmpID),
 	    foreign key (SkillID) references SkillSet (SkillID),
         foreign key (weaponID) references WeaponsData (SerialNo)
@@ -70,13 +70,13 @@ CREATE TABLE AlphaAgents
 
 --
 CREATE TABLE TechnicalSkills 
-        (TechID varchar(15),
-        NameOfTheSkill varchar(155),
+        (TechID varchar(156),
+        NameOfTheSkill varchar(256),
         RequiredProficiencyLevel numeric(4,0),
         HasCertifications varchar(3),
-        DifficultyLevel varchar(3),
-        PrerequisiteSkills varchar(155),
-        whoSkilledIn varchar(155),
+        DifficultyLevel varchar(155),
+        PrerequisiteSkills varchar(256),
+        whoSkilledIn varchar(256),
         primary key (TechID),
         foreign Key (whoSkilledIn) references BetaAgents(EmpID)
         );
@@ -84,87 +84,83 @@ CREATE TABLE TechnicalSkills
 -- CREATE TABLE Passport
 
 --         (
---         PassportNo varchar(155),
---         Nationality varchar(15),
---         ExpirationDate varchar(15),
---         IssuedDate varchar(15),
+--         PassportNo varchar(256),
+--         Nationality varchar(156),
+--         ExpirationDate varchar(156),
+--         IssuedDate varchar(156),
 --         );
 
 --
 -- CREATE TABLE Languages
     
 --         (
---         languageID varchar(155),
---         MotherTongue varchar(155),
---         Preference1 varchar(155),
---         Preference2 varchar(155),
---         Preference3 varchar(155),
---         KnowExtraLanguages varchar(155),
---         spokenBy varchar(15),
+--         languageID varchar(256),
+--         MotherTongue varchar(256),
+--         Preference1 varchar(256),
+--         Preference2 varchar(256),
+--         Preference3 varchar(256),
+--         KnowExtraLanguages varchar(256),
+--         spokenBy varchar(156),
 --         );
 
 --
 CREATE TABLE OperationDirector
-        (OperationId varchar(155),
-        OperationDirectorName varchar(155),
-        OperationName varchar(155),
-        OperationLocation varchar(155),
+        (OperationId varchar(256),
+        OperationDirectorName varchar(256),
+        OperationName varchar(256),
+        OperationLocation varchar(256),
+        passwords varchar(255),
         primary key (OperationId)
         );
 --
 CREATE TABLE TerroristsInformation
-        (IdentityNumber varchar(15),
-        NameOfTerrorists varchar(15),
-        aliases varchar(15),
-        Height numeric(2,2),
-        color varchar(15),
-        ethnicity varchar(15),
-        locationsWhereHeWasFound varchar(155),
+        (IdentityNumber varchar(156),
+        NameOfTerrorists varchar(156),
+        aliases varchar(156),
+        Height numeric(4),
+        color varchar(156),
+        ethnicity varchar(156),
+        locationsWhereHeWasFound varchar(256),
         InCustody varchar(3),
-        missedToCatch varchar(3),
-        CanBeKilled varchar(3),
-        InformationSentTo varchar(155),
-        CaughtBy varchar(15),
+        InformationSentTo varchar(256),
+        ConfimredBy varchar(156),
+        SuspectedBy varchar(156),
         primary key (IdentityNumber),
-	    foreign key (CaughtBy) references AlphaAgents(EmpID),
-        foreign key (CaughtBy) references BetaAgents(EmpID),
+	foreign key (SuspectedBy) references AlphaAgents(EmpID),
+        foreign key (ConfimredBy) references BetaAgents(EmpID),
         foreign key (InformationSentTo) references OperationDirector(OperationId)
         );
-
 --   
 CREATE TABLE Budget
-        (EventName varchar(155),
-        dateTimes varchar(15),
+        (EventName varchar(256),
+        dateTimes varchar(156),
         MoneySpentOnEvent numeric(7,0),
-        QuoteSubmittedBy varchar(15),
-        AuthorisedBy varchar(15),
+        QuoteSubmittedBy varchar(156),
+        AuthorisedBy varchar(156),
         primary key (EventName,dateTimes),
         foreign key (QuoteSubmittedBy) references AlphaAgents(EmpID),
         foreign key (AuthorisedBy) references OperationDirector(OperationId)
         );
-
 --
 CREATE TABLE Informers
-        (IDNumber varchar(155),
-        Name varchar(155),
-        aliases varchar(155),
-        occupation varchar(155),
+        (IDNumber varchar(256),
+        Name varchar(256),
+        aliases varchar(256),
+        occupation varchar(256),
         AccuracyLevel numeric(2,0),
-        recruitedBy varchar(15),
+        recruitedBy varchar(156),
         primary key (IDNumber),
         foreign key (recruitedBy) references BetaAgents(EmpID)
         );
-
 --
 CREATE TABLE ElectronicDevices
-        (SerialNumber varchar(155),
-        NameOfDevise varchar(155),
-        model varchar(155),
-        MfdYear numeric(4,0),
+        (SerialNumber varchar(256),
+        NameOfDevise varchar(256),
+        model varchar(256),
         Price numeric(6,0),
         requiresExpInHandling varchar(3),
         requiresMaintenance varchar(3),
-        UsedBy varchar(15),
+        UsedBy varchar(156),
         primary key (SerialNumber),
         foreign key (UsedBy) references BetaAgents(EmpID)
         );
@@ -181,37 +177,35 @@ CREATE TABLE ElectronicDevices
 
 --
 CREATE TABLE TerroristActivities
-        (dateTimes varchar(15),
-        Locations varchar(155),
+        (dateTimes varchar(156),
+        Locations varchar(256),
         numberOfPeopleKilled numeric(7,0),
-        Clues varchar(155),
-        Report varchar(155),
-        InspectedBy varchar(155),
+        Report varchar(256),
+        InspectedBy varchar(256),
         primary key (dateTimes, Locations),
         foreign key (InspectedBy) references AlphaAgents(EmpID)
         );
-
 --
 CREATE TABLE Causalities
-        (dateTimes varchar(15),
-        DeceasedName varchar(155),
-        DeceasedHomeAddress varchar(155),
-        DeceasedNationality varchar(155),
-        Location varchar(155),
-        AssetDamageInDollars varchar(155),
-        primary key (dateTimes, Location, DeceasedName),
-        foreign key (dateTimes, Location) references TerroristActivities (dateTimes, Locations)
+        (dateTimes varchar(156),
+        incidentLocation varchar(256),
+        DeceasedName varchar(256),
+        DeceasedHomeAddress varchar(256),
+        DeceasedNationality varchar(256),
+        AssetDamageInDollars varchar(256),
+        primary key (dateTimes, incidentLocation, DeceasedName),
+        foreign key (dateTimes, incidentLocation) references TerroristActivities (dateTimes, Locations)
         );
 
 -- 
 CREATE TABLE Patrolling
-        (PatrollingID varchar(155),
-        dateTimes varchar(15),
-        Location varchar(155),
-        VehicleUsed varchar(155),
-        purposeOfPatrolling varchar(155),
-        DriverName varchar(155),
-        officerOnDuty varchar(15),
+        (PatrollingID varchar(256),
+        dateTimes varchar(256),
+        Location varchar(256),
+        VehicleUsed varchar(256),
+        purposeOfPatrolling varchar(256),
+        DriverName varchar(256),
+        officerOnDuty varchar(256),
         primary key (PatrollingID),
         foreign key (officerOnDuty) references AlphaAgents(EmpID)
         );
